4c3462b9177da95dc0ebe5d0a63b9707b92aaedb
MbeHenri <henri.mbe@facsciences-uy1.cm>
2024-05-11 12:20:43 +0100

    handle user connection to mongodb

--- opencare-patient-backend/config.js
+++ opencare-patient-backend/config.js
@@ -10,7 +10,7 @@ const FRONTEND_HOST = process.env.FRONTEND_HOST;
 const FRONTEND_PORT = process.env.FRONTEND_PORT;
 
 module.exports = {
-  dbURI: `mongodb://${MONGO_USER && MONGO_PASSWORD ? `${MONGO_USER}:${MONGO_PASSWORD}@` : ""}${MONGO_HOST}${MONGO_PORT ? `:${MONGO_PORT}` : ""}/opencare`,
+  dbURI: `mongodb://${(MONGO_USER && MONGO_PASSWORD) ? `${MONGO_USER}:${MONGO_PASSWORD}@` : ""}${MONGO_HOST}${MONGO_PORT ? `:${MONGO_PORT}` : ""}/opencare?authSource=admin`,
   FRONTEND: `http://${FRONTEND_HOST}${FRONTEND_PORT ? `:${FRONTEND_PORT}` : ""}`,
   port: process.env.SERVER_PORT ? parseInt(process.env.SERVER_PORT) : 3001,
   key_token: process.env.KEY_TOKEN ? process.env.KEY_TOKEN : ""
